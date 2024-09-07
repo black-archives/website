c88010053afcfbc04d7f09df7ab7f318d8ca49b8
Oliver Manzi <olivermanzi.dev@gmail.com>
2024-09-08 00:06:47 +0200

    patch: adds/cleans up standard theme files

--- wordpress/wp-content/themes/highwire/page.php
+++ wordpress/wp-content/themes/highwire/page.php
@@ -1,3 +1,9 @@
+<!-- 
+ This is a template for page templates. Simply copy the contents
+ to a new file in the same directory and name it something like
+page-about.php. Then you can edit the content as you see fit.
+ -->
+
 <?php get_header(); ?>
 
 <main role="main">
--- wordpress/wp-content/themes/highwire/single.php
+++ wordpress/wp-content/themes/highwire/single.php
@@ -9,6 +9,7 @@
     }
     ?>
   </div>
+
   <div class="wrap-s">
     <?php if (have_posts()): while (have_posts()) : the_post(); ?>
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
