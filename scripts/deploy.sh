e51fe232714336fc9ef33428a7af0b938df99714
Oliver Manzi <olivermanzi.dev@gmail.com>
2024-09-15 20:17:22 +0200

    patch: adjusts deployment script/workflow

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,13 +9,29 @@ CURRENT_DIR="${CURRENT_DIR}/.." # Move up one directory
 
 ## CONSTANTS ############################################
 
+SERVER_HOSTNAME=$1
+SERVER_USERNAME=$2
+SERVER_KEY_PATH=$3
+
 SOURCE_THEME_PATH="${CURRENT_DIR}/wordpress/wp-content/themes/highwire/*"
 TARGET_THEME_PATH="/www/wp-content/themes/highwire/"
 
 ## MAIN ##################################################
 
-read -p "Enter Server Hostname: " SERVER_HOSTNAME
-read -p "Enter Server Username: " SERVER_USERNAME
+if [ -z "$SERVER_HOSTNAME" ] || [ -z "$SERVER_USERNAME" ]; then
+  read -p "Enter Server Hostname: " SERVER_HOSTNAME
+  read -p "Enter Server Username: " SERVER_USERNAME
+fi
+
+if [ -z "$SERVER_KEY_PATH" ]; then
+  read -p "Enter Server Key Path (optional): " SERVER_KEY_PATH
+fi
+
 
 echo "Deploying theme to $SERVER_HOSTNAME..."
-scp -r $SOURCE_THEME_PATH $SERVER_USERNAME@$SERVER_HOSTNAME:$TARGET_THEME_PATH
+
+if [ -z "$SERVER_KEY_PATH" ]; then
+  scp -r $SOURCE_THEME_PATH $SERVER_USERNAME@$SERVER_HOSTNAME:$TARGET_THEME_PATH
+else
+  scp -i $SERVER_KEY_PATH -r $SOURCE_THEME_PATH $SERVER_USERNAME@$SERVER_HOSTNAME:$TARGET_THEME_PATH
+fi
