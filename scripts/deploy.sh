f0e780dff6fdb800766adf45ba34c8e566dddff6
oliverrr <32515201+this-oliver@users.noreply.github.com>
2024-09-15 20:44:44 +0200

    patch: adjusts deployment script/workflow (#9)
    * patch: adjusts deployment script/workflow
    * patch: wraps user inputs
    * patch: skips interactive key check for workflow

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,13 +9,33 @@ CURRENT_DIR="${CURRENT_DIR}/.." # Move up one directory
 
 ## CONSTANTS ############################################
 
+_SERVER_HOSTNAME=$1                         # hostname for server
+_SERVER_USERNAME=$2                         # username for server
+_SERVER_KEY_PATH=$3                         # path to the server key
+_CI_MODE=$4                                 # if "true", then don't check for strict host key checking
+
 SOURCE_THEME_PATH="${CURRENT_DIR}/wordpress/wp-content/themes/highwire/*"
 TARGET_THEME_PATH="/www/wp-content/themes/highwire/"
 
 ## MAIN ##################################################
 
-read -p "Enter Server Hostname: " SERVER_HOSTNAME
-read -p "Enter Server Username: " SERVER_USERNAME
+if [ -z "${_SERVER_HOSTNAME}" ] || [ -z "${_SERVER_USERNAME}" ]; then
+  read -p "Enter Server Hostname: " _SERVER_HOSTNAME
+  read -p "Enter Server Username: " _SERVER_USERNAME
+fi
 
-echo "Deploying theme to $SERVER_HOSTNAME..."
-scp -r $SOURCE_THEME_PATH $SERVER_USERNAME@$SERVER_HOSTNAME:$TARGET_THEME_PATH
+if [ -z "${_SERVER_KEY_PATH}" ]; then
+  read -p "Enter Server Key Path (optional): " _SERVER_KEY_PATH
+fi
+
+echo "Deploying theme to ${_SERVER_HOSTNAME}..."
+
+if [ -z "$_SERVER_KEY_PATH" ]; then
+  scp -r ${SOURCE_THEME_PATH} ${_SERVER_USERNAME}@${_SERVER_HOSTNAME}:${TARGET_THEME_PATH}
+else
+  if [[ "$_CI_MODE" = "true" || "$_CI_MODE" = "TRUE" || "$_CI_MODE" = "1" ]]; then
+    scp -o StrictHostKeyChecking=no -i ${_SERVER_KEY_PATH} -r ${SOURCE_THEME_PATH} ${_SERVER_USERNAME}@${_SERVER_HOSTNAME}:${TARGET_THEME_PATH}
+  else
+    scp -i ${_SERVER_KEY_PATH} -r ${SOURCE_THEME_PATH} ${_SERVER_USERNAME}@${_SERVER_HOSTNAME}:${TARGET_THEME_PATH}
+  fi
+fi
