c17e8938f1630af653f4a40bca1806c2d34fb937
Oliver Manzi <olivermanzi.dev@gmail.com>
2024-09-29 12:35:01 +0100

    patch: renames and documents page-parent template

--- src/page-parent-children.php
+++ src/page-parent-children.php
@@ -1,13 +1,21 @@
-<?php /* Template Name: Parent Page */ ?>
+<?php
+/* Template Name: Parent Children Page */
+
+//This template is for parent pages. It will display all child pages of the parent page in a grid layout.
+
+//Pre-requisites:
+//- Create a new page in WordPress and assign this template to it.
+//- Create child pages for the parent page.
+//- Add a featured image to each child page. If no featured image is added, a fallback image will be used.
+//- Add a summary to each child page in the 'Page Summary' field of the Custom Fields section. If no summary is added, the excerpt will be used.
+?>
 
 <?php get_header(); ?>
-<main role="main" class="parent-page">
+<main role="main" class="parent-children-page">
   <section class="front-heading">
     <div class="col-3-4">
       <h1><?= get_the_title(); ?></h1>
-      <p>
-        <?= get_the_content(); ?>
-      </p>
+      <p><?= get_the_content(); ?></p>
     </div>
   </section>
 
@@ -18,20 +26,20 @@
       $args = array(
         'post_parent' => $post->ID,
         'posts_per_page' => -1,
-        'post_type' => 'page', //you can use also 'any'
+        'post_type' => 'page',
       );
-
-      // set call to actions (if in /support path, then set it to 'support', if in /submit path, then set it to 'submit' otherwise set it to 'view')
-      $call_to_action = 'View';
-      if (strpos($_SERVER['REQUEST_URI'], '/support') !== false) {
-        $call_to_action = 'Support';
-      } elseif (strpos($_SERVER['REQUEST_URI'], '/submit') !== false) {
-        $call_to_action = 'Submit';
-      }
 
       $the_query = new WP_Query($args);
 
       if ($the_query->have_posts()):
+        // set call to actions (if in /support path, then set it to 'support', if in /submit path, then set it to 'submit' otherwise set it to 'view')
+        $call_to_action = 'View';
+        if (strpos($_SERVER['REQUEST_URI'], '/support') !== false) {
+          $call_to_action = 'Support';
+        } elseif (strpos($_SERVER['REQUEST_URI'], '/submit') !== false) {
+          $call_to_action = 'Submit';
+        }
+
         while ($the_query->have_posts()) : $the_query->the_post();
           $title = get_the_title();
           $subtitle = get_field('page_summary') ?: get_the_excerpt();
