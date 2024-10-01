6694749bcb231e4436e856f48e1d109118251469
Oliver Manzi <olivermanzi.dev@gmail.com>
2024-10-01 12:55:05 +0100

    patch: removes buttons from cards in parent-children template
    - except for submit page

--- src/page-parent-children.php
+++ src/page-parent-children.php
@@ -3,17 +3,13 @@
 /**
  * Template Name: Parent Children Page
  * 
- * This template is for parent pages. It will display all child pages of the parent page in a grid layout.
- * 
- * Pre-requisites:
- * - Create a new page in WordPress and assign this template to it.
- * - Create child pages for the parent page.
- * - Add a featured image to each child page. If no featured image is added, a fallback image will be used.
- * - Add a summary to each child page in the 'Page Summary' field of the Custom Fields section. If no summary is added, the excerpt will be used.
+ * This template displays the contents of the current page and lists all child
+ * pages of the current page in a grid layout.
  */
 
 get_header();
 ?>
+
 <main role="main" class="parent-children-page">
   <section class="front-heading">
     <div class="col-3-4">
@@ -35,14 +31,6 @@ get_header();
       $the_query = new WP_Query($args);
 
       if ($the_query->have_posts()):
-        // set call to actions (if in /support path, then set it to 'support', if in /submit path, then set it to 'submit' otherwise set it to 'view')
-        $call_to_action = 'View';
-        if (strpos($_SERVER['REQUEST_URI'], '/support') !== false) {
-          $call_to_action = 'Support';
-        } elseif (strpos($_SERVER['REQUEST_URI'], '/submit') !== false) {
-          $call_to_action = 'Submit';
-        }
-
         while ($the_query->have_posts()) : $the_query->the_post();
           $title = get_the_title();
           $subtitle = get_field('page_summary') ?: get_the_excerpt();
@@ -57,7 +45,6 @@ get_header();
 
                 <div class="bottom">
                   <p><?php echo esc_html($subtitle); ?></p>
-                  <button class="btn btn-primary" style="padding-bottom: 15px;"><?php echo esc_html($call_to_action); ?> <img src="/wp-content/uploads/2021/03/Pil.svg" /></button>
                 </div>
               </div>
             </a>
@@ -88,7 +75,6 @@ get_header();
 
               <div class="bottom">
                 <p><?= $card_summary ?></p>
-                <button class="btn btn-primary" style="padding-bottom: 15px;"><?= $card_call_to_action ?> <img src="/wp-content/uploads/2021/03/Pil.svg" /></button>
               </div>
             </div>
           </div>
