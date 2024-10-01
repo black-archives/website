a5e36955a6bbc5c4f5976147aa34f4cd93ac9a92
Oliver Manzi <olivermanzi.dev@gmail.com>
2024-10-01 21:22:41 +0100

    patch: adjusts footer styles

--- src/footer.php
+++ src/footer.php
@@ -6,12 +6,10 @@
  */
 ?>
 
-<div class="bottom-footer">
-  <div class="copyright">
-    <p>&copy; <?php echo date('Y'); ?> All rights reserved </p>
-  </div>
+<div class="bottom-footer tw-flex tw-justify-between">
+  <p>&copy; <?php echo date('Y'); ?> All rights reserved </p>
 
-  <div class="newsletter-form">
+  <div>
     <?php if (ICL_LANGUAGE_CODE == 'en'): ?>
       <?= do_shortcode("[cm_form form_id='cm_64833d96cf335']"); ?>
     <?php elseif (ICL_LANGUAGE_CODE == 'sv'): ?>
