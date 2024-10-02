b1c036ea7a8dbfd4f338c5a693cc084ffe6a52f5
Oliver Manzi <olivermanzi.dev@gmail.com>
2024-10-02 12:18:23 +0100

    fix: sidebar language component misbehaving
    - large texts
    - misaligned searchbar (with respect to lang-list)

--- src/header.php
+++ src/header.php
@@ -52,28 +52,39 @@
     <div class="main-header">
       <!-- sidebar -->
       <div class="left-menu">
-        <div class="menu-hamburger">
+        <div class="menu-hamburger tw-flex tw-place-items-center">
           <img src="/wp-content/uploads/2021/03/Meny.svg" alt="Menu icon">
         </div>
         <img class="header-logo" src="/wp-content/uploads/2021/03/logo-side.svg" alt="Black Archive Sweden Logo">
       </div>
 
       <!-- sidebar active content -->
-      <nav class="nav" role="navigation">
-        <div class="meny-cross"><img src="/wp-content/uploads/2021/03/Meny-kryss.svg" alt="Close menu cross"></div>
-
-        <a href="<?php echo get_home_url(); ?>"><img class="menu-logo" src="/wp-content/uploads/2021/03/BAS-Logga.svg" alt="Black Archive Sweden Logo"></a>
-
-        <?php html5blank_nav(); ?>
-
-        <div class="nav-footer flex flex-space vert-center" style="    margin-top: 15px;">
-          <form class="search" method="get" action="<?php echo home_url(); ?>" role="search">
-            <input class="search-input" type="search" name="s" placeholder="<?php _e('Search', 'html5blank'); ?>">
-          </form>
-          <?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-area-2')) ?>
+      <nav class="nav tw-flex tw-flex-col tw-space-y-5" role="navigation">
+        <div class="tw-grow-0 meny-cross">
+          <img src="/wp-content/uploads/2021/03/Meny-kryss.svg" alt="Close menu cross">
         </div>
 
-        <div class="social-icons">
+        <a class="tw-my-5 tw-grow-0" href="<?php echo get_home_url(); ?>">
+          <img class="menu-logo" src="/wp-content/uploads/2021/03/BAS-Logga.svg" alt="Black Archive Sweden Logo">
+        </a>
+
+        <div class="tw-grow">
+          <?php html5blank_nav(); ?>
+        </div>
+
+        <div class="nav-footer tw-mt-5 tw-grow-0 tw-flex tw-space-x-3">
+          <div class="tw-basis-1/2 tw-flex tw-place-items-center">
+            <form class="search" method="get" action="<?php echo home_url(); ?>" role="search">
+              <input class="search-input" type="search" name="s" placeholder="<?php _e('Search', 'html5blank'); ?>">
+            </form>
+          </div>
+
+          <div class="lang-list tw-basis-1/2 tw-pl-5">
+            <?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-area-2')) ?>
+          </div>
+        </div>
+
+        <div class="social-icons tw-mt-5 tw-grow-0">
           <?php while (has_sub_field('social_media', 'option')): ?>
             <a href="<?php the_sub_field('url'); ?>" target="_blank"><?php the_sub_field('icons'); ?></a>
           <?php endwhile; ?>
