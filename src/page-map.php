698ae0319445dfdc3b3a98b1157ac15b78a3486e
Oliver Manzi <olivermanzi.dev@gmail.com>
2024-10-08 21:29:28 +0200

    patch: removes map css

--- src/page-map.php
+++ src/page-map.php
@@ -61,6 +61,12 @@ function get_map_story_by_id($id)
     <div
       id="map-container"
       class="tw-h-svh md:tw-h-screen-90vh md:tw-grow tw-border-black tw-border-2 md:tw-border-0">
+      <!-- 10004px × 7087px (scaled to 1146px × 811px) -->
+      <svg
+        id="map-svg"
+        class="tw-w-full tw-h-full"
+        xmlns="http://www.w3.org/2000/svg">
+        <g id="map-svg-group">
           <!-- background -->
           <image href="<?= $map_image_url; ?>" />
 
@@ -93,9 +99,10 @@ function get_map_story_by_id($id)
     </div>
 
     <!-- options -->
-    <div id="map-options" class="md:tw-grow-0 tw-my-2 tw-mx-1 md:tw-mx-10 tw-space-y-2 md:tw-space-y-0 tw-flex tw-flex-col md:tw-flex-row md:tw-justify-between">
+    <div
+      id="map-options" class="tw-absolute tw-bottom-0 tw-inset-x-0 tw-w-full md:tw-w-auto md:tw-relative md:tw-grow-0 tw-my-2 tw-mx-1 md:tw-mx-10 tw-space-y-2 md:tw-space-y-0 tw-flex tw-flex-col md:tw-flex-row tw-justify-between">
       <!-- interaction options -->
-      <div class="tw-flex tw-justify-center md:tw-justify-start tw-space-x-2">
+      <div class="tw-flex md:tw-justify-start tw-space-x-2">
         <button id="btn-zoom-in" class="tw-px-2 tw-py-1 tw-border tw-border-black tw-bg-white hover:tw-bg-slate-800 hover:tw-text-white">
           zoom in
         </button>
@@ -105,38 +112,37 @@ function get_map_story_by_id($id)
       </div>
 
       <!-- extra options -->
-      <div class="tw-flex tw-justify-center md:tw-justify-end tw-space-x-2">
-        <button class="tw-px-2 tw-py-1 tw-border tw-border-black tw-bg-white hover:tw-bg-slate-800 hover:tw-text-white">
+      <div class="tw-flex md:tw-justify-end tw-space-x-2">
+        <button class="tw-grow-0 tw-px-2 tw-py-1 tw-border tw-border-black tw-bg-white hover:tw-bg-slate-800 hover:tw-text-white">
           En
         </button>
-        <button class="tw-px-2 tw-py-1 tw-border tw-border-black tw-bg-white hover:tw-bg-slate-800 hover:tw-text-white">
+        <button class="tw-grow-0 tw-px-2 tw-py-1 tw-border tw-border-black tw-bg-white hover:tw-bg-slate-800 hover:tw-text-white">
           Sv
         </button>
-        <button id="map-open-modal" class="tw-px-2 tw-py-1 tw-border tw-border-black tw-bg-white hover:tw-bg-slate-800 hover:tw-text-white">
+        <button id="map-open-modal" class="tw-grow md:tw-grow-0 tw-px-2 tw-py-1 tw-border tw-border-black tw-bg-white hover:tw-bg-slate-800 hover:tw-text-white">
           Info
         </button>
       </div>
     </div>
-  </div>
 
-  <div id="map-modal" class="modal">
-    <div class="modal-content tw-flex-col">
-      <span class="modal-close ">
-        <img class="tw-ml-auto tw-mr-3 tw-my-3 tw-cursor-pointer" src="/wp-content/uploads/2021/03/Meny-kryss.svg" alt="Button to close modal">
-      </span>
-      <section id="project-info" class="tw-flex-col tw-justify-center">
-        <div class="md:tw-w-2/6 md:tw-mx-auto tw-text-center">
-          <h1 style="<?= $title_font_size; ?>">
-            <?= get_the_title(); ?>
-          </h1>
-        </div>
+    <div id="map-modal" class="modal">
+      <div class="modal-content tw-flex-col">
+        <span class="modal-close ">
+          <img class="tw-ml-auto tw-mr-3 tw-my-3 tw-cursor-pointer" src="/wp-content/uploads/2021/03/Meny-kryss.svg" alt="Button to close modal">
+        </span>
+        <section id="project-info" class="tw-flex-col tw-justify-center">
+          <div class="md:tw-w-2/6 md:tw-mx-auto tw-text-center">
+            <h1 style="<?= $title_font_size; ?>">
+              <?= get_the_title(); ?>
+            </h1>
+          </div>
 
-        <div class="tw-mt-5 md:tw-w-1/2 md:tw-mx-auto">
-          <?= get_the_content(); ?>
-        </div>
-      </section>
+          <div class="tw-mt-5 md:tw-w-1/2 md:tw-mx-auto">
+            <?= get_the_content(); ?>
+          </div>
+        </section>
+      </div>
     </div>
-  </div>
 </main>
 
 <?php get_footer(); ?>
