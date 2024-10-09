17eb2612be98e3b657626f072d04142ff9ec1511
oliverrr <32515201+this-oliver@users.noreply.github.com>
2024-10-09 20:43:18 +0200

    fix: events page was not responsive (#19)

--- src/archive-events.php
+++ src/archive-events.php
@@ -19,34 +19,40 @@ get_header();
   </section>
 
   <section class="post-feed">
-    <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-        <div class="tw-flex tw-flex-row-reverse" style="border-bottom: 1px solid #000;">
-          <div class="tw-basis-1/2">
-            <?php the_post_thumbnail(); ?>
-          </div>
-
-          <div class="event-info tw-basis-1/2 tw-flex tw-flex-col tw-place-content-between tw-p-5">
-            <div class="top tw-flex-col">
-              <p class="date flex flex-space">
-                <span><?php the_field('day'); ?></span>
-                <span><?php the_field('time'); ?></span>
-              </p>
-              <p class="heading"><?php the_field('date'); ?></p>
+    <div class="tw-flex-col">
+      <?php
+      if (have_posts()):
+        while (have_posts()) :
+          the_post();
+      ?>
+          <div class="tw-flex tw-flex-col md:tw-flex-row-reverse" style="border-bottom: 1px solid #000;">
+            <div class="md:tw-basis-1/2">
+              <?php the_post_thumbnail(); ?>
             </div>
 
-            <div class="bottom tw-flex-col">
-              <div>
-                <h2><?php the_title(); ?></h2>
-                <?php custom_length_excerpt(40); ?>
+            <div class="event-info tw-flex tw-flex-col tw-place-content-between tw-p-5 md:tw-basis-1/2">
+              <div class="top tw-flex-col">
+                <p class="date flex flex-space">
+                  <span><?php the_field('day'); ?></span>
+                  <span><?php the_field('time'); ?></span>
+                </p>
+                <p class="heading"><?php the_field('date'); ?></p>
               </div>
-              <?php if (get_field('link')): ?>
-                <p><a href="<?php the_field('link'); ?>" class="btn btn-primary" style="margin-top: 40px;"><?php the_field('link_text'); ?><img src="/wp-content/uploads/2021/03/Pil.svg" alt="Go to event arrow"></a></p>
-              <?php endif; ?>
+
+              <div class="bottom tw-flex-col">
+                <div class="tw-overflow-hidden tw-text-wrap tw-text-ellipsis">
+                  <h2><?php the_title(); ?></h2>
+                  <?php custom_length_excerpt(40); ?>
+                </div>
+                <?php if (get_field('link')): ?>
+                  <p><a href="<?php the_field('link'); ?>" class="btn btn-primary" style="margin-top: 40px;"><?php the_field('link_text'); ?><img src="/wp-content/uploads/2021/03/Pil.svg" alt="Go to event arrow"></a></p>
+                <?php endif; ?>
+              </div>
             </div>
           </div>
-        </div>
-      <?php endwhile; ?>
-    <?php endif; ?>
+        <?php endwhile; ?>
+      <?php endif; ?>
+    </div>
   </section>
 </main>
 <?php get_footer(); ?>
