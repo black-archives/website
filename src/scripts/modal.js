ca10a3a4b366757b5d863bb657c96d7ec0e7d953
oliverrr <32515201+this-oliver@users.noreply.github.com>
2024-10-10 22:51:36 +0200

    patch: adds some pre-checks to modal.js (#24)

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -7,17 +7,23 @@ document.addEventListener("DOMContentLoaded", function () {
 	var modal = document.getElementById("map-modal");
 	var span = document.getElementsByClassName("modal-close")[0];
 
-	btn.onclick = function () {
-		modal.style.display = "block";
-	};
+	if (btn && modal) {
+		btn.onclick = function () {
+			modal.style.display = "block";
+		};
+	}
 
-	span.onclick = function () {
-		modal.style.display = "none";
-	};
-
-	window.onclick = function (event) {
-		if (event.target == modal) {
+	if (span && modal) {
+		span.onclick = function () {
 			modal.style.display = "none";
-		}
-	};
+		};
+	}
+
+	if (modal) {
+		window.onclick = function (event) {
+			if (event.target == modal) {
+				modal.style.display = "none";
+			}
+		};
+	}
 });
