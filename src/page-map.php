ba60e108f78f928c33165b2ab2fc8273ebd89a86
oliverrr <32515201+this-oliver@users.noreply.github.com>
2024-10-11 20:09:49 +0200

    fix: minor corrections to map page (#25)
    * fix: adds coords for pointer 25
    * fix: language buttons not reacting on touch screens
    * patch: centers initial map
    * patch: swaps map info modal for section below map

--- src/page-map.php
+++ src/page-map.php
@@ -29,277 +29,276 @@ $map_image_url = get_field('map_image');
 $map_stories = get_field('map_stories');
 
 // Create an array of 2 PHP objects with dummy data
-$pointer_color = "red";
 $map_pointers = [
   (object) [
     'id' => 1,
     'x' => '7746.912109375',
     'y' => '2287.798583984375',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 2,
     'x' => '7902.91552734375',
     'y' => '2220.940185546875',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 3,
     'x' => '8155.4921875',
     'y' => '2354.656982421875',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 4,
     'x' => '7709.7685546875',
     'y' => '2503.2314453125',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 5,
     'x' => '7932.63037109375',
     'y' => '2532.9462890625',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 6,
     'x' => '7405.19091796875',
     'y' => '3023.241943359375',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 7,
     'x' => '7486.9072265625',
     'y' => '3015.813232421875',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 8,
     'x' => '7761.76953125',
     'y' => '4620.4169921875',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 9,
     'x' => '7858.34326171875',
     'y' => '4627.845703125',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 10,
     'x' => '7264.04541015625',
     'y' => '3216.388671875',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 11,
     'x' => '7449.763671875',
     'y' => '4419.841796875',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 12,
     'x' => '7085.755859375',
     'y' => '3045.528076171875',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 13,
     'x' => '7197.18701171875',
     'y' => '3038.099365234375',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 14,
     'x' => '310.76214599609375',
     'y' => '5341.0029296875',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 15,
     'x' => '7769.1982421875',
     'y' => '3231.24609375',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 16,
     'x' => '7925.20166015625',
     'y' => '4523.84375',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 17,
     'x' => '7256.61669921875',
     'y' => '4018.690673828125',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 18,
     'x' => '7249.18798828125',
     'y' => '4115.26416015625',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 19,
     'x' => '4760.56640625',
     'y' => '5066.14013671875',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 20,
     'x' => '7940.05908203125',
     'y' => '3075.242919921875',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 21,
     'x' => '7249.18798828125',
     'y' => '4211.83740234375',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 22,
     'x' => '7732.0546875',
     'y' => '4085.549072265625',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 23,
     'x' => '7286.33154296875',
     'y' => '3104.9580078125',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 24,
     'x' => '7791.48486328125',
     'y' => '3535.82373046875',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 25,
-    'x' => '0',
-    'y' => '0',
-    'color' => '#39ff14',
+    'x' => '7538.908203125',
+    'y' => '3669.540771484375',
+
   ],
   (object) [
     'id' => 26,
     'x' => '7590.9091796875',
     'y' => '3877.544921875',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 27,
     'x' => '7204.61572265625',
     'y' => '2941.526123046875',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 28,
     'x' => '8816.6484375',
     'y' => '2733.521728515625',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 29,
     'x' => '8898.3642578125',
     'y' => '2674.092041015625',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 30,
     'x' => '7576.0517578125',
     'y' => '4360.41162109375',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 31,
     'x' => '9024.65234375',
     'y' => '2703.806884765625',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 32,
     'x' => '7776.626953125',
     'y' => '3929.5458984375',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 33,
     'x' => '6536.03076171875',
     'y' => '4033.548095703125',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 34,
     'x' => '6075.4501953125',
     'y' => '3877.544921875',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 35,
     'x' => '8110.91943359375',
     'y' => '2191.22509765625',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 36,
     'x' => '7278.90283203125',
     'y' => '2837.52392578125',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 37,
     'x' => '7821.19970703125',
     'y' => '4107.83544921875',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 38,
     'x' => '7888.05810546875',
     'y' => '4419.841796875',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 39,
     'x' => '7427.47705078125',
     'y' => '3268.389892578125',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 40,
     'x' => '7873.20068359375',
     'y' => '4196.97998046875',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 41,
     'x' => '8408.068359375',
     'y' => '2889.52490234375',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 42,
     'x' => '4337.12939453125',
     'y' => '4612.98828125',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 43,
     'x' => '7516.6220703125',
     'y' => '3231.24609375',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 44,
     'x' => '7784.05615234375',
     'y' => '3832.97265625',
-    'color' => $pointer_color,
+
   ],
   (object) [
     'id' => 45,
     'x' => '7531.4794921875',
     'y' => '3104.9580078125',
-    'color' => $pointer_color,
+
   ]
 ];
 
@@ -434,28 +433,28 @@ function sanitize_text($text)
         <button id="btn-lang-sv" class="tw-basis-1/4 <?= $menu_option_btn_style; ?>">
           Sv
         </button>
-        <button id="map-open-modal" class="tw-basis-2/4 <?= $menu_option_btn_style; ?>">
+        <button id="btn-map-info" class="tw-basis-2/4 <?= $menu_option_btn_style; ?>">
           Info
         </button>
       </div>
     </div>
 
-    <div id="map-modal" class="modal">
-      <div class="modal-content tw-flex-col">
-        <span class="modal-close ">
-          <img class="tw-ml-auto tw-mr-3 tw-my-3 tw-cursor-pointer" src="/wp-content/uploads/2021/03/Meny-kryss.svg" alt="Button to close modal">
-        </span>
-        <section id="project-info" class="tw-flex-col tw-justify-center">
-          <div class="md:tw-w-2/6 md:tw-mx-auto tw-text-center">
-            <h1 style="<?= $title_font_size; ?>">
-              <?= get_the_title(); ?>
-            </h1>
-          </div>
+    <!-- info -->
+    <div id="map-info" class="tw-my-10 tw-px-5 tw-h-svh tw-flex tw-flex-col tw-justify-center md:tw-my-10">
+      <div class="md:tw-w-2/6 md:tw-mx-auto tw-text-center">
+        <h1 style="<?= $title_font_size; ?>">
+          <?= get_the_title(); ?>
+        </h1>
+      </div>
 
-          <div class="tw-mt-5 md:tw-w-1/2 md:tw-mx-auto">
-            <?= get_the_content(); ?>
-          </div>
-        </section>
+      <div class="tw-mt-5 md:tw-w-1/2 md:tw-mx-auto">
+        <?= get_the_content(); ?>
+      </div>
+
+      <div class="tw-mt-5 md:tw-w-1/2 md:tw-mx-auto">
+        <button id="btn-map-info-close" class="tw-px-5 tw-py-2 tw-text-xl tw-rounded-full tw-border tw-border-black tw-bg-white">
+          Map
+        </button>
       </div>
     </div>
 </main>
--- src/scripts/map.js
+++ src/scripts/map.js
@@ -1,244 +1,31 @@
 /**
  * This script is responsible for setting up the interactive map in
- * page-map.php.
+ * page-map.php file.
  *
- * It uses the panzoom library to enable zooming and panning of the map image
- * within the map svg. It also creates a card element that displays information
- * about a point on the map when the point is clicked.
+ * The script does the following:
+ *
+ * - It uses the panzoom library to pand and zoom the [map] image within the svg.
+ * - Creates card elements that displays information about map pointers when the user clicks on them.
+ * - Switches the language of the map card content when the user clicks on the language buttons.
+ * - Scrolls between the map and the map info section when the user clicks on the map info buttons.
  */
+
+// ======================================== Variables ========================================
 
 const map = document.getElementById("map-main"); // entire map container
 const mapSvg = document.getElementById("map-svg");
 const mapSvgGroup = document.getElementById("map-svg-group");
+const mapInfo = document.getElementById("map-info");
+const mapInfoBtn = document.getElementById("btn-map-info");
+const mapInfoCloseBtn = document.getElementById("btn-map-info-close");
 const zoomInBtn = document.getElementById("btn-zoom-in");
 const zoomOutBtn = document.getElementById("btn-zoom-out");
 const englishLanguageBtn = document.getElementById("btn-lang-en");
 const swedishLanguageBtn = document.getElementById("btn-lang-sv");
 
-const points = [
-	{
-		id: 1,
-		x: 7746.912109375,
-		y: 2287.798583984375,
-	},
-	{
-		id: 2,
-		x: 7902.91552734375,
-		y: 2220.940185546875,
-	},
-	{
-		id: 3,
-		x: 8155.4921875,
-		y: 2354.656982421875,
-	},
-	{
-		id: 4,
-		x: 7709.7685546875,
-		y: 2503.2314453125,
-	},
-	{
-		id: 5,
-		x: 7932.63037109375,
-		y: 2532.9462890625,
-	},
-	{
-		id: 6,
-		x: 7405.19091796875,
-		y: 3023.241943359375,
-	},
-	{
-		id: 7,
-		x: 7486.9072265625,
-		y: 3015.813232421875,
-	},
-	{
-		id: 8,
-		x: 7761.76953125,
-		y: 4620.4169921875,
-	},
-	{
-		id: 9,
-		x: 7858.34326171875,
-		y: 4627.845703125,
-	},
-	{
-		id: 10,
-		x: 7264.04541015625,
-		y: 3216.388671875,
-	},
-	{
-		id: 11,
-		x: 7449.763671875,
-		y: 4419.841796875,
-	},
-	{
-		id: 12,
-		x: 7085.755859375,
-		y: 3045.528076171875,
-	},
-	{
-		id: 13,
-		x: 7197.18701171875,
-		y: 3038.099365234375,
-	},
-	{
-		id: 14,
-		x: 310.76214599609375,
-		y: 5341.0029296875,
-	},
-	{
-		id: 15,
-		x: 7769.1982421875,
-		y: 3231.24609375,
-	},
-	{
-		id: 16,
-		x: 7925.20166015625,
-		y: 4523.84375,
-	},
-	{
-		id: 17,
-		x: 7256.61669921875,
-		y: 4018.690673828125,
-	},
-	{
-		id: 18,
-		x: 7249.18798828125,
-		y: 4115.26416015625,
-	},
-	{
-		id: 19,
-		x: 4760.56640625,
-		y: 5066.14013671875,
-	},
-	{
-		id: 20,
-		x: 7940.05908203125,
-		y: 3075.242919921875,
-	},
-	{
-		id: 21,
-		x: 7249.18798828125,
-		y: 4211.83740234375,
-	},
-	{
-		id: 22,
-		x: 7732.0546875,
-		y: 4085.549072265625,
-	},
-	{
-		id: 23,
-		x: 7286.33154296875,
-		y: 3104.9580078125,
-	},
-	{
-		id: 24,
-		x: 7791.48486328125,
-		y: 3535.82373046875,
-	},
-	{
-		id: 25,
-		x: 7590.9091796875,
-		y: 3877.544921875,
-	},
-	{
-		id: 26,
-		x: 7204.61572265625,
-		y: 2941.526123046875,
-	},
-	{
-		id: 27,
-		x: 8816.6484375,
-		y: 2733.521728515625,
-	},
-	{
-		id: 28,
-		x: 8898.3642578125,
-		y: 2674.092041015625,
-	},
-	{
-		id: 29,
-		x: 7576.0517578125,
-		y: 4360.41162109375,
-	},
-	{
-		id: 30,
-		x: 9024.65234375,
-		y: 2703.806884765625,
-	},
-	{
-		id: 31,
-		x: 7776.626953125,
-		y: 3929.5458984375,
-	},
-	{
-		id: 32,
-		x: 6536.03076171875,
-		y: 4033.548095703125,
-	},
-	{
-		id: 33,
-		x: 6075.4501953125,
-		y: 3877.544921875,
-	},
-	{
-		id: 34,
-		x: 8110.91943359375,
-		y: 2191.22509765625,
-	},
-	{
-		id: 35,
-		x: 7278.90283203125,
-		y: 2837.52392578125,
-	},
-	{
-		id: 36,
-		x: 7821.19970703125,
-		y: 4107.83544921875,
-	},
-	{
-		id: 37,
-		x: 7888.05810546875,
-		y: 4419.841796875,
-	},
-	{
-		id: 38,
-		x: 7427.47705078125,
-		y: 3268.389892578125,
-	},
-	{
-		id: 39,
-		x: 7873.20068359375,
-		y: 4196.97998046875,
-	},
-	{
-		id: 40,
-		x: 8408.068359375,
-		y: 2889.52490234375,
-	},
-	{
-		id: 41,
-		x: 7516.6220703125,
-		y: 3231.24609375,
-	},
-	{
-		id: 42,
-		x: 7784.05615234375,
-		y: 3832.97265625,
-	},
-	{
-		id: 43,
-		x: 4337.12939453125,
-		y: 4612.98828125,
-	},
-	{
-		id: 44,
-		x: 7531.4794921875,
-		y: 3104.9580078125,
-	},
-];
-
 let selectedCardElement = null;
+
+// ======================================== Functions ========================================
 
 /**
  * Returns true if browser is a mobile device
@@ -356,12 +143,9 @@ function setupPanzoom() {
 	const instance = panzoom(mapSvgGroup, {
 		bounds: true,
 		boundsPadding: isMobile() ? 0.05 : 0.9, // the bigger the value (max 1), the less of the map is visible
-
-		maxZoom: 0.5,
-		minZoom: 0.1,
-		initialZoom: isMobileDevice ? 0.2 : 0.3,
-		initialX: isMobileDevice ? -300 : -300,
-		initialY: isMobileDevice ? 0 : 100,
+		initialZoom: isMobileDevice ? 0.15 : 0.2,
+		initialX: isMobileDevice ? -1100 : 0,
+		initialY: isMobileDevice ? 0 : 0,
 	});
 
 	// increase scale of map when zoom in button is clicked
@@ -417,19 +201,41 @@ function setLanguage(lang) {
 	window.location.href = `${origin}${newPath}`;
 }
 
+// ======================================== Event Listeners ========================================
+
 // setup the panzoom instance when the document is loaded
 document.addEventListener("DOMContentLoaded", setupPanzoom);
 
-// add event listener to the english language button
-if (englishLanguageBtn) {
-	englishLanguageBtn.addEventListener("click", function () {
-		setLanguage("en");
+// add event listener to map info button to scroll to the map info section
+if (mapInfoBtn) {
+	mapInfoBtn.addEventListener("click", function () {
+		if (mapInfo) {
+			mapInfo.scrollIntoView({ behavior: "smooth" });
+		}
 	});
 }
 
-// add event listener to the swedish language button
-if (swedishLanguageBtn) {
-	swedishLanguageBtn.addEventListener("click", function () {
-		setLanguage("sv");
+// add event listener to map info close button to scroll to the map
+if (mapInfoCloseBtn) {
+	mapInfoCloseBtn.addEventListener("click", function () {
+		if (mapSvg) {
+			mapSvg.scrollIntoView({ behavior: "smooth" });
+		}
 	});
 }
+
+// add event listener to language buttons
+const events = ["click", "touchend"];
+events.forEach((event) => {
+	if (englishLanguageBtn) {
+		englishLanguageBtn.addEventListener(event, function () {
+			setLanguage("en");
+		});
+	}
+
+	if (swedishLanguageBtn) {
+		swedishLanguageBtn.addEventListener(event, function () {
+			setLanguage("sv");
+		});
+	}
+});
