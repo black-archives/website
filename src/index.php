0de5c9fa21fa822f3618f7ebbdcfbc36b3ab651f
Oliver Manzi <olivermanzi.dev@gmail.com>
2024-11-28 22:17:12 +0100

    adds custom field with highlight repeaters

--- src/index.php
+++ src/index.php
@@ -72,124 +72,130 @@ get_header();
       displays the items in the archive page.
       */
 
-      // Reset Post Data
-      wp_reset_postdata();
-
-      $pinned_items_key = "pinned_items";
-
-      while (have_rows($pinned_items_key)) : the_row();
-        $item_title = get_field('title');
-        $item_body = get_field('body');
+      while (have_rows("pinned_items")) : the_row();
+        $item_title = get_sub_field('title');
+        $item_body = get_sub_field('body');
+        $item_image = get_sub_field('image');
+        $item_cta = get_sub_field('cta'); // array with 'url' and 'title'
 
       ?>
-        <div class="col-f-1-3 active" style="background: #35A270;">
-          <div class="box-content">
-
-            <div class="post-text-box">
-              <div class="top">
-                <h2><?= $item_title; ?></h2>
-              </div>
-
-              <div class="bottom">
-                <p><?= $item_body ?></p>
-              </div>
-            </div>
-            </a>
-          </div>
-        <?php endwhile; ?>
-
-        <div class="col-f-1-3 ">
+        <div class="col-f-1-3 active">
           <div class="box-content hover-active">
-            <a href="https://www.blackarchivessweden.com/index-of-archival-projects-sites/">
-              <img src="https://www.blackarchivessweden.com/wp-content/uploads/2023/06/BAS_index_ig_1.jpg" alt="">
+            <a href="<?= $item_cta['url']; ?>" style="height: 100%;">
+              <img src="<?= $item_image; ?>" alt="">
+              <div class="post-hover-box">
+                <div class="top">
+                  <h2><?= $item_title; ?></h2>
+                </div>
+
+                <div class="bottom tw-mt-auto tw-w-full">
+                  <?php if ($item_body) : ?>
+                    <p><?= $item_body; ?></p>
+                  <?php endif; ?>
+
+                  <button class="btn btn-primary tw-w-full tw-flex">
+                    <?= $item_cta['title']; ?>
+                    <img src="/wp-content/uploads/2021/03/Pil.svg" />
+                  </button>
+                </div>
+              </div>
             </a>
           </div>
         </div>
+      <?php endwhile; ?>
 
-        <?php
-        $args = array(
-          'posts_per_page'    => -1,
-          'post_type'     => 'post'
-        );
-        // query
-        $the_query = new WP_Query($args);
-
-
-        if ($the_query->have_posts()):
-          while ($the_query->have_posts()) : $the_query->the_post(); ?>
-            <?php $args = array(
-              'exclude' => '1'
-            );
-            $categories = get_the_category();
-            ?>
-            <div class="col-f-1-3 post-boxes active <?php
-                                                    foreach ($categories as $category) {
-                                                      echo $category->slug . " ";
-                                                    } ?> ">
-              <div class="box-content <?php if (has_post_thumbnail()): echo 'hover-active';
-                                      endif; ?> ">
-                <a href="<?php the_permalink(); ?>" style="height: 100%;">
-                  <?php if (has_post_thumbnail()):
-                    the_post_thumbnail(); ?>
-                    <div class="post-hover-box">
-                      <div class="top">
-                        <h2><?php the_title(); ?></h2>
-                      </div>
-
-                      <div class="bottom">
-                        <p class="show-desktop"><?php if (get_field('short_about')):  echo get_text_excerpt(get_field('short_about'));
-                                                else: custom_length_excerpt(20);
-                                                endif;   ?></p>
-                        <p class="show-ipad"><?php if (get_field('short_about')):  echo get_text_excerpt(get_field('short_about'), 10);
-                                              else: custom_length_excerpt(8);
-                                              endif;   ?></p>
-                        <ul class="cat-list">
-                          <?php
-
-                          foreach ($categories as $category) {
-                            if ($category->name !== 'All') {
-                              echo "<li>" . $category->name . "</li>";
-                            }
-                          } ?>
-                        </ul>
-                      </div>
-                    </div>
-                  <?php else: ?>
-                    <div class="post-text-box">
-                      <div class="top">
-                        <h2><?php the_title(); ?></h2>
-                      </div>
-                      <div class="bottom">
-                        <p class="show-desktop"><?php if (get_field('short_about')):  echo get_text_excerpt(get_field('short_about'));
-                                                else: custom_length_excerpt(20);
-                                                endif;   ?></p>
-                        <p class="show-ipad"><?php if (get_field('short_about')):  echo get_text_excerpt(get_field('short_about'), 10);
-                                              else: custom_length_excerpt(8);
-                                              endif;   ?></p>
-                        <ul class="cat-list">
-                          <?php
-                          $args = array(
-                            'exclude' => '1'
-                          );
-                          $categories = get_categories($args);
-                          foreach ($categories as $category) {
-                            if ($category->name !== 'All') {
-                              echo "<li>" . $category->name . "</li>";
-                            }
-                          } ?>
-                        </ul>
-                      </div>
-
-                    </div>
-                  <?php endif; ?>
-                </a>
-              </div>
-            </div>
-        <?php endwhile;
-        else :
-        endif;
-        ?>
+      <div class="col-f-1-3 ">
+        <div class="box-content hover-active">
+          <a href="https://www.blackarchivessweden.com/index-of-archival-projects-sites/">
+            <img src="https://www.blackarchivessweden.com/wp-content/uploads/2023/06/BAS_index_ig_1.jpg" alt="">
+          </a>
         </div>
+      </div>
+
+      <?php
+      $args = array(
+        'posts_per_page'    => -1,
+        'post_type'     => 'post'
+      );
+      // query
+      $the_query = new WP_Query($args);
+
+
+      if ($the_query->have_posts()):
+        while ($the_query->have_posts()) : $the_query->the_post(); ?>
+          <?php $args = array(
+            'exclude' => '1'
+          );
+          $categories = get_the_category();
+          ?>
+          <div class="col-f-1-3 post-boxes active <?php
+                                                  foreach ($categories as $category) {
+                                                    echo $category->slug . " ";
+                                                  } ?> ">
+            <div class="box-content <?php if (has_post_thumbnail()): echo 'hover-active';
+                                    endif; ?> ">
+              <a href="<?php the_permalink(); ?>" style="height: 100%;">
+                <?php if (has_post_thumbnail()):
+                  the_post_thumbnail(); ?>
+                  <div class="post-hover-box">
+                    <div class="top">
+                      <h2><?php the_title(); ?></h2>
+                    </div>
+
+                    <div class="bottom">
+                      <p class="show-desktop"><?php if (get_field('short_about')):  echo get_text_excerpt(get_field('short_about'));
+                                              else: custom_length_excerpt(20);
+                                              endif;   ?></p>
+                      <p class="show-ipad"><?php if (get_field('short_about')):  echo get_text_excerpt(get_field('short_about'), 10);
+                                            else: custom_length_excerpt(8);
+                                            endif;   ?></p>
+                      <ul class="cat-list">
+                        <?php
+
+                        foreach ($categories as $category) {
+                          if ($category->name !== 'All') {
+                            echo "<li>" . $category->name . "</li>";
+                          }
+                        } ?>
+                      </ul>
+                    </div>
+                  </div>
+                <?php else: ?>
+                  <div class="post-text-box">
+                    <div class="top">
+                      <h2><?php the_title(); ?></h2>
+                    </div>
+                    <div class="bottom">
+                      <p class="show-desktop"><?php if (get_field('short_about')):  echo get_text_excerpt(get_field('short_about'));
+                                              else: custom_length_excerpt(20);
+                                              endif;   ?></p>
+                      <p class="show-ipad"><?php if (get_field('short_about')):  echo get_text_excerpt(get_field('short_about'), 10);
+                                            else: custom_length_excerpt(8);
+                                            endif;   ?></p>
+                      <ul class="cat-list">
+                        <?php
+                        $args = array(
+                          'exclude' => '1'
+                        );
+                        $categories = get_categories($args);
+                        foreach ($categories as $category) {
+                          if ($category->name !== 'All') {
+                            echo "<li>" . $category->name . "</li>";
+                          }
+                        } ?>
+                      </ul>
+                    </div>
+
+                  </div>
+                <?php endif; ?>
+              </a>
+            </div>
+          </div>
+      <?php endwhile;
+      else :
+      endif;
+      ?>
+    </div>
   </section>
 </main>
 
