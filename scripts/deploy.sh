88666bf2ee2cd21581cc72fe4d25df87192de7e3
Oliver Manzi <olivermanzi.dev@gmail.com>
2025-03-29 10:29:27 +0100

    patch: updates deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,9 +12,8 @@ ROOT_DIR=$(realpath "${CURRENT_DIR}/..") # Move up one directory
 _SERVER_HOSTNAME=$1                         # hostname for server
 _SERVER_USERNAME=$2                         # username for server
 _SERVER_KEY_PATH=$3                         # path to the server key
-_CI_MODE=$4                                 # if "true", then don't check for strict host key checking
 
-SOURCE_THEME_PATH="${ROOT_DIR}/src/*"
+SOURCE_THEME_PATH="${ROOT_DIR}/src/"
 TARGET_THEME_PATH="/www/wp-content/themes/highwire/"
 
 ## MAIN ##################################################
@@ -30,22 +29,14 @@ if [ -z "${_SERVER_KEY_PATH}" ]; then
   read -p "Enter Server Key Path (optional): " _SERVER_KEY_PATH
 fi
 
-echo "Deploying theme to ${_SERVER_HOSTNAME}..."
+echo "\n"
+echo "Target Server: ${_SERVER_USERNAME}@${_SERVER_HOSTNAME}"
+echo "Target Theme Path: ${TARGET_THEME_PATH}"
+echo "Source Theme Path: ${SOURCE_THEME_PATH}"
 
 # if the server key path is not provided, use the default ssh command
 if [ -z "$_SERVER_KEY_PATH" ]; then
-  ssh ${_SERVER_USERNAME}@${_SERVER_HOSTNAME} "rm -rf ${TARGET_THEME_PATH}/*"
-  scp -r ${SOURCE_THEME_PATH} ${_SERVER_USERNAME}@${_SERVER_HOSTNAME}:${TARGET_THEME_PATH}
+  rsync -avz --delete "${SOURCE_THEME_PATH}" "${_SERVER_USERNAME}@${_SERVER_HOSTNAME}:${TARGET_THEME_PATH}"
 else
-
-  # if the CI mode is enabled, then don't check for strict host key checking
-  if [[ "$_CI_MODE" = "true" || "$_CI_MODE" = "TRUE" || "$_CI_MODE" = "1" ]]; then
-    ssh -o StrictHostKeyChecking=no -i ${_SERVER_KEY_PATH} ${_SERVER_USERNAME}@${_SERVER_HOSTNAME} "rm -rf ${TARGET_THEME_PATH}/*"
-    scp -o StrictHostKeyChecking=no -i ${_SERVER_KEY_PATH} -r ${SOURCE_THEME_PATH} ${_SERVER_USERNAME}@${_SERVER_HOSTNAME}:${TARGET_THEME_PATH}
-
-  # otherwise, use the custom key path
-  else
-    ssh -i ${_SERVER_KEY_PATH} ${_SERVER_USERNAME}@${_SERVER_HOSTNAME} "rm -rf ${TARGET_THEME_PATH}/*"
-    scp -i ${_SERVER_KEY_PATH} -r ${SOURCE_THEME_PATH} ${_SERVER_USERNAME}@${_SERVER_HOSTNAME}:${TARGET_THEME_PATH}
-  fi
+  rsync -avz --delete -e "ssh -i \"${_SERVER_KEY_PATH}\"" "${SOURCE_THEME_PATH}" "${_SERVER_USERNAME}@${_SERVER_HOSTNAME}:${TARGET_THEME_PATH}"
 fi
